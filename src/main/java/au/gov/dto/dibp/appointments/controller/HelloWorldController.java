aae604cb8d284c3fba1cf76cebee814100e493c3
Marzena Sokolowska <msokolo@thoughtworks.com>
2015-12-04 16:39:53 +1100

    Hello World sample

--- src/main/java/au/gov/dto/dibp/appointments/controller/HelloWorldController.java
+++ src/main/java/au/gov/dto/dibp/appointments/controller/HelloWorldController.java
@@ -12,20 +12,20 @@ import java.util.Map;
 public class HelloWorldController {
     @RequestMapping(value = "/hello.txt", method = RequestMethod.GET, produces = "text/plain")
     public String hello() {
-        return "Hello world";
+        return "Hello world!";
     }
 
     @RequestMapping(value = "/hello.json", method = RequestMethod.GET, produces = "application/json")
     public Map<String, Object> helloJson() {
         return new HashMap<String, Object>() {{
-            put("hello", "world");
+            put("hello", "world!");
         }};
     }
 
     @RequestMapping(value = "/hello.html", method = RequestMethod.GET, produces = "text/html")
     public ModelAndView helloHtml() {
         return new ModelAndView("helloworld", new HashMap<String, Object>() {{
-            put("hello", "world");
+            put("hello", "world!");
         }});
     }
 }
