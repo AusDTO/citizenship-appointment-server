3a7be5c280ab1f99ddb27b624d42259bd80f3821
Emily Siow <emily.siow@digital.gov.au>
2015-12-17 17:51:00 +1100

    Show stack trace in browser

--- src/main/java/au/gov/dto/dibp/appointments/Application.java
+++ src/main/java/au/gov/dto/dibp/appointments/Application.java
@@ -1,9 +1,12 @@
 package au.gov.dto.dibp.appointments;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude=ErrorMvcAutoConfiguration.class)
 public class Application {
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
