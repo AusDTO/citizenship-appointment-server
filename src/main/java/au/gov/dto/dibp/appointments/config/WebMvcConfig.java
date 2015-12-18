d93791f29df419547a18246b6cedcc6cfef7467d
Emily Siow <emily.siow@digital.gov.au>
2015-12-18 16:24:11 +1100

    Configure MustacheViewResolver as default view resolver

--- src/main/java/au/gov/dto/dibp/appointments/config/WebMvcConfig.java
+++ src/main/java/au/gov/dto/dibp/appointments/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package au.gov.dto.dibp.appointments.config;
 
+import org.springframework.boot.autoconfigure.mustache.web.MustacheViewResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,5 +12,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/login");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(new MustacheViewResolver());
     }
 }
