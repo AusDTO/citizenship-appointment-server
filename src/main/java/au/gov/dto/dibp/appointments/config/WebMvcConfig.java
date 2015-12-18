ab8239f90dbc6d3062c9e4d8dbc32ee01a91062f
Emily Siow <emily.siow@digital.gov.au>
2015-12-18 11:23:23 +1100

    Setup root route

--- src/main/java/au/gov/dto/dibp/appointments/config/WebMvcConfig.java
+++ src/main/java/au/gov/dto/dibp/appointments/config/WebMvcConfig.java
@@ -1,10 +1,14 @@
 package au.gov.dto.dibp.appointments.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/login");
+    }
 }
