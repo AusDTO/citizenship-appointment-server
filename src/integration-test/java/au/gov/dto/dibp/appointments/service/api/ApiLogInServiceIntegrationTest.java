b4f799c2e67ecbd0dfe5a2d03d6fe71d81d25ec4
Emily Siow <emily.siow@digital.gov.au>
2015-12-21 17:15:09 +1100

    Test successful login to QFlow API

--- src/integration-test/java/au/gov/dto/dibp/appointments/service/api/ApiLogInServiceIntegrationTest.java
+++ src/integration-test/java/au/gov/dto/dibp/appointments/service/api/ApiLogInServiceIntegrationTest.java
@@ -9,18 +9,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 
-public class ApiLoginServiceIntegrationTest {
+public class ApiLogInServiceIntegrationTest {
 
     private final DefaultResourceLoader defaultResourceLoader = new DefaultResourceLoader();
     private MockWebServer mockWebServer;
@@ -33,19 +30,19 @@ public class ApiLoginServiceIntegrationTest {
                 try {
                     String requestBody = request.getBody().readUtf8();
                     if (requestBody.contains("success_user")) {
-                        String responseBody = IOUtils.toString(defaultResourceLoader.getResource("classpath:FormsSignInSuccessResponse.xml").getInputStream(), StandardCharsets.UTF_8);
+                        String responseBody = IOUtils.toString(new FileReader("src/integration-test/resources/FormsSignInSuccessResponse.xml"));
                         return new MockResponse().setResponseCode(200).setBody(responseBody);
                     }
                     if (requestBody.contains("wrong_credentials_user")) {
-                        String responseBody = IOUtils.toString(defaultResourceLoader.getResource("classpath:FormsSignInWrongCredentialsResponse.xml").getInputStream(), StandardCharsets.UTF_8);
+                        String responseBody = IOUtils.toString(new FileReader("src/integration-test/resources/FormsSignInWrongCredentialsResponse.xml"));
                         return new MockResponse().setResponseCode(500).setBody(responseBody);
                     }
                     if (requestBody.contains("taken_user")) {
-                        String responseBody = IOUtils.toString(defaultResourceLoader.getResource("classpath:FormsSignInTakenUserResponse.xml").getInputStream(), StandardCharsets.UTF_8);
+                        String responseBody = IOUtils.toString(new FileReader("src/integration-test/resources/FormsSignInTakenUserResponse.xml"));
                         return new MockResponse().setResponseCode(500).setBody(responseBody);
                     }
 
-                    return null;
+                    throw new RuntimeException("Unexpected request: " + requestBody);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -61,9 +58,9 @@ public class ApiLoginServiceIntegrationTest {
     }
 
     @Test
-    public void testSuccessfulApiLogin() throws SAXException, ParserConfigurationException, XPathExpressionException, IOException {
-//        ApiLoginService apiLoginService = new ApiLoginService(new DefaultResourceLoader(), new ApiUserService(), new HttpClientHandler(), "","");
-//        String apiSessionId = apiLoginService.login();
-//        assertThat(apiSessionId, not(isEmptyOrNullString()));
+    public void testSuccessfulApiLogin() throws Exception {
+        ApiLoginService apiLoginService = new ApiLoginService(new DefaultResourceLoader(), new ApiUserService(new ApiUser("success_user", "any_password")), new HttpClientHandler(), "http://localhost:"+this.mockWebServer.getPort(), "false");
+        String apiSessionId = apiLoginService.login();
+        assertThat(apiSessionId, not(isEmptyOrNullString()));
     }
 }
--- src/main/java/au/gov/dto/dibp/appointments/service/api/ApiUserService.java
+++ src/main/java/au/gov/dto/dibp/appointments/service/api/ApiUserService.java
@@ -6,22 +6,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ApiUserService {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final List<ApiUser> apiUsers;
 
-    public List<ApiUser> initializeApiUsers(){
+    public ApiUserService() {
         int userCount = Integer.parseInt(getEnvironmentVariable("USER_COUNT"));
-        List<ApiUser> apiUsers = new ArrayList<>();
+        apiUsers = new ArrayList<>();
 
         for(int i = 1; i<=userCount; i++){
             String username = getEnvironmentVariable("USER_USERNAME_"+i);
             String password = getEnvironmentVariable("USER_PASSWORD_"+i);
             apiUsers.add(new ApiUser(username, password));
         }
-        return apiUsers;
+    }
+
+    public ApiUserService(ApiUser... apiUsers) {
+        this.apiUsers = Arrays.asList(apiUsers);
+    }
+
+    public List<ApiUser> initializeApiUsers(){
+        return new ArrayList<>(apiUsers);
     }
 
     String getEnvironmentVariable(String name){
