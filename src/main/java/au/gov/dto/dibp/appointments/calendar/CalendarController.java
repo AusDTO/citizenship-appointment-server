9b483e37e1f44408c8e73e3c4221c96e0d4bffc9
Marzena Sokolowska <msokolo@thoughtworks.com>
2016-01-08 16:47:33 +1100

    Display current appointment time on the Calendar page

--- src/main/java/au/gov/dto/dibp/appointments/calendar/CalendarController.java
+++ src/main/java/au/gov/dto/dibp/appointments/calendar/CalendarController.java
@@ -1,26 +1,47 @@
 package au.gov.dto.dibp.appointments.calendar;
 
+import au.gov.dto.dibp.appointments.appointmentdetails.AppointmentDetails;
+import au.gov.dto.dibp.appointments.appointmentdetails.AppointmentDetailsService;
+import au.gov.dto.dibp.appointments.client.Client;
+import au.gov.dto.dibp.appointments.unit.UnitDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class CalendarController {
 
+    private final AppointmentDetailsService appointmentDetailsService;
+    private final UnitDetailsService unitDetailsService;
+
+    @Autowired
+    public CalendarController(AppointmentDetailsService appointmentDetailsService, UnitDetailsService unitDetailsService){
+        this.appointmentDetailsService = appointmentDetailsService;
+        this.unitDetailsService = unitDetailsService;
+    }
+
     @RequestMapping(value = "/booking", method = RequestMethod.GET, produces = "text/html")
-    public ModelAndView bookingHtml(@RequestParam(value = "error", required = false) String error, HttpServletRequest request) {
+    public ModelAndView bookingHtml(@AuthenticationPrincipal Client client,
+                                    @RequestParam(value = "error", required = false) String error,
+                                    HttpServletRequest request) {
 
         Map<String, Object> model = new HashMap<>();
 
-        model.put("location", "Ground Floor, 26 Lee St");
-        model.put("todayDate", LocalDate.now(ZoneId.of("Australia/Sydney")));
+        //TODO: refactor not to make 2 calls for unitId & details
+        model.put("location", getUnitLocation(client));
+        model.put("todayDate", getUnitCurrentDate(client));
+        model.put("current_appointment", getCurrentAppointmentDetails(client));
+
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
         if (csrfToken != null) {
             model.put("_csrf", csrfToken);
@@ -29,6 +50,22 @@ public class CalendarController {
             model.put("error", true);
         }
         return new ModelAndView("booking_page", model);
+    }
+
+    private String getCurrentAppointmentDetails(Client client){
+        AppointmentDetails appointment = appointmentDetailsService.getExpectedAppointmentForClientForNextYear(client);
+        if (appointment != null) {
+            return appointment.getAppointmentDate().format(DateTimeFormatter.ofPattern("EEEE dd MMMM, h:mm a"));
+        }
+        return null;
+    }
+
+    private String getUnitLocation(Client client){
+        return unitDetailsService.getUnitAddressByServiceId(client.getServiceId());
+    }
+
+    private String getUnitCurrentDate(Client client){
+        return unitDetailsService.getUnitCurrentLocalTimeByServiceId(client.getServiceId());
     }
 
 }
---
+++ src/test/java/au/gov/dto/dibp/appointments/calendar/CalendarControllerTest.java
@@ -0,0 +1,93 @@
+package au.gov.dto.dibp.appointments.calendar;
+
+import au.gov.dto.dibp.appointments.appointmentdetails.AppointmentDetails;
+import au.gov.dto.dibp.appointments.appointmentdetails.AppointmentDetailsService;
+import au.gov.dto.dibp.appointments.client.Client;
+import au.gov.dto.dibp.appointments.unit.UnitDetailsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class CalendarControllerTest {
+
+    private static final String SERVICE_ID = "5";
+    private static final String UNIT_ADDRESS = "Some Street 12, Melbourne";
+    private static final String UNIT_LOCALTIME = "2016-01-10T11:20:00";
+    private static final String CURRENT_APPOINTMENT_TIME = "2016-01-18T13:20:00";
+
+    private CalendarController controller;
+
+    @Before
+    public void setUp(){
+        controller = new CalendarController(new AppointmentDetailsService(null, null, null){
+            @Override
+            public AppointmentDetails getExpectedAppointmentForClientForNextYear(Client client) {
+                return getBasicAppointmentDetails();
+            }
+        }, new UnitDetailsService(null, null, null){
+
+            @Override
+            public String getUnitCurrentLocalTimeByServiceId(String serviceId){
+                assertThat(serviceId, is(SERVICE_ID));
+                return UNIT_LOCALTIME;
+            }
+
+            @Override
+            public String getUnitAddressByServiceId(String serviceId){
+                assertThat(serviceId, is(SERVICE_ID));
+                return UNIT_ADDRESS;
+            }
+        });
+    }
+
+    @Test
+    public void test_bookingHtml_should_passTheLocationToTheModel(){
+        final ModelAndView result = controller.bookingHtml(getStandardClient(), null, mock(HttpServletRequest.class));
+
+        assertThat(result.getModel().get("location"), is(UNIT_ADDRESS));
+    }
+
+    @Test
+    public void test_bookingHtml_should_passTodayDateToTheModel(){
+        final ModelAndView result = controller.bookingHtml(getStandardClient(), null, mock(HttpServletRequest.class));
+
+        assertThat(result.getModel().get("todayDate"), is(UNIT_LOCALTIME));
+    }
+
+    @Test
+    public void test_bookingHtml_should_passCurrentUserAppointmentToTheModel(){
+        final ModelAndView result = controller.bookingHtml(getStandardClient(), null, mock(HttpServletRequest.class));
+
+        assertThat(result.getModel().get("current_appointment"), is("Monday 18 January, 1:20 PM"));
+    }
+
+    @Test
+    public void test_bookingHtml_should_passErrorToTheModelIfPresent(){
+        final ModelAndView result = controller.bookingHtml(getStandardClient(), "Some error", mock(HttpServletRequest.class));
+
+        assertThat(result.getModel().get("error"), is(true));
+    }
+
+    @Test
+    public void test_bookingHtml_should_notPopulateErrorInTheModelIfNotPresent(){
+        final ModelAndView result = controller.bookingHtml(getStandardClient(), null, mock(HttpServletRequest.class));
+
+        assertThat(result.getModel().get("error"), is(nullValue()));
+    }
+
+    private Client getStandardClient (){
+        return new Client("123", "Surname", "40404", true, true);
+    }
+
+    private AppointmentDetails getBasicAppointmentDetails(){
+        return new AppointmentDetails(LocalDateTime.parse(CURRENT_APPOINTMENT_TIME), 20, "121212", SERVICE_ID, "3333", "Sydney", "Some Street 12");
+    }
+}
