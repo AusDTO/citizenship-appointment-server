a7c0f0395c65bc7a8d42405c34d3513e26bae0bb
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-08 17:38:25 +1100

    Ensure Spring Security doesn't create HttpSession

--- src/main/java/au/gov/dto/dibp/appointments/config/WebSecurityConfig.java
+++ src/main/java/au/gov/dto/dibp/appointments/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -30,6 +31,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
             .securityContext()
                 .securityContextRepository(cookieBasedSecurityContextRepository)
                 .and()
