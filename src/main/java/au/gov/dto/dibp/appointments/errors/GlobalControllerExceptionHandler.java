e8bac92b923e4a38ba45b188b25b578668fb2bf9
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-19 22:01:11 +1100

    Ensure exceptions are logged when handled by the global handler

--- src/main/java/au/gov/dto/dibp/appointments/errors/GlobalControllerExceptionHandler.java
+++ src/main/java/au/gov/dto/dibp/appointments/errors/GlobalControllerExceptionHandler.java
@@ -1,17 +1,21 @@
 package au.gov.dto.dibp.appointments.errors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
     @ExceptionHandler(RuntimeException.class)
-    public ModelAndView handleRuntimeException(HttpServletRequest req, RuntimeException exception){
+    public ModelAndView handleRuntimeException(RuntimeException exception) {
+        LOGGER.error("Unhandled RuntimeException", exception);
         return new ModelAndView("redirect:/error", new HashMap<>());
     }
 }
