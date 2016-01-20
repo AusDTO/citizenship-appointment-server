6623568cfaec57fbc5565298c8aeebe2fcdf731b
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-20 23:22:01 +1100

    Error page returns HTTP 500 status code

--- src/main/java/au/gov/dto/dibp/appointments/errors/ErrorsController.java
+++ src/main/java/au/gov/dto/dibp/appointments/errors/ErrorsController.java
@@ -1,11 +1,13 @@
 package au.gov.dto.dibp.appointments.errors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 @Controller
@@ -13,12 +15,14 @@ public class ErrorsController {
 
     private final String trackingId;
 
+    @Autowired
     public ErrorsController(@Value("${analytics.tracking.id}") String trackingId) {
         this.trackingId = trackingId;
     }
 
     @RequestMapping(value = "/error", method = RequestMethod.GET, produces = "text/html")
-    public ModelAndView getErrorPage() {
+    public ModelAndView getErrorPage(HttpServletResponse response) {
+        response.setStatus(500);
         HashMap<String, Object> model = new HashMap<>();
         model.put("trackingId", trackingId);
         return new ModelAndView("error_page", model);
