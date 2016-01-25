770afdf29082689b0733bfea4e0ca6bd65b1b702
Michael Richardson <michael.richardson@digital.gov.au>
2016-01-25 14:36:46 +1100

    production deployment to DTO PCF

---
+++ bin/cideploy-prod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit immediately if any commands return non-zero
+set -e
+# Output the commands we run
+set -x
+
+# This is a modified version of the Cloud Foundry Blue/Green deployment guide:
+#
+# https://docs.pivotal.io/pivotalcf/devguide/deploy-apps/blue-green.html
+
+# Update the blue app
+cf unmap-route citizenship-appointment-beta-blue appointments.border.gov.au 
+cf push citizenship-appointment-beta-blue --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
+cf map-route citizenship-appointment-beta-blue appointments.border.gov.au  
+
+# Update the green app
+cf unmap-route citizenship-appointment-beta-green appointments.border.gov.au
+cf push citizenship-appointment-beta-green --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
+cf map-route citizenship-appointment-beta-green appointments.border.gov.au 
+
