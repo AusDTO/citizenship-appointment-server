e90d8df2b0cf69025917dc5250a5926d3487646e
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-28 16:42:42 +1100

    Fix admin authentication

--- src/main/java/au/gov/dto/dibp/appointments/errors/ErrorsController.java
+++ src/main/java/au/gov/dto/dibp/appointments/errors/ErrorsController.java
@@ -3,6 +3,7 @@ package au.gov.dto.dibp.appointments.errors;
 import au.gov.dto.dibp.appointments.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 @Controller
-public class ErrorsController {
+public class ErrorsController implements ErrorController {
+    private static final String ERROR_PATH = "/error";
 
     private final String trackingId;
 
@@ -22,14 +24,21 @@ public class ErrorsController {
         this.trackingId = trackingId;
     }
 
-    @RequestMapping(value = "/error", method = RequestMethod.GET, produces = "text/html")
+    @RequestMapping(value = ERROR_PATH, method = RequestMethod.GET, produces = "text/html")
     public ModelAndView getErrorPage(@AuthenticationPrincipal Client client, HttpServletResponse response) {
-        response.setStatus(500);
+        if (response.getStatus() == 200) {
+            response.setStatus(500);
+        }
         HashMap<String, Object> model = new HashMap<>();
         model.put("trackingId", trackingId);
         if (client != null) {
             model.put("clientId", client.getClientId());
         }
         return new ModelAndView("error_page", model);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
     }
 }
