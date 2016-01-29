f55c08520fee20ac7d2809cd775609634d82d866
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-29 15:50:16 +1100

    Use DoSFilter

--- src/main/java/au/gov/dto/dibp/appointments/config/AppConfig.java
+++ src/main/java/au/gov/dto/dibp/appointments/config/AppConfig.java
@@ -3,22 +3,45 @@ package au.gov.dto.dibp.appointments.config;
 import au.gov.dto.dibp.appointments.initializer.HttpsOnlyFilter;
 import au.gov.dto.dibp.appointments.initializer.LogClientIdFilter;
 import au.gov.dto.dibp.appointments.initializer.NoHttpSessionFilter;
+import au.gov.dto.dibp.appointments.initializer.DoSFilter;
 import com.oakfusion.security.SecurityCookieService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 import javax.servlet.http.HttpSessionListener;
 
 @Configuration
 @ComponentScan(basePackages = "au.gov.dto.dibp.appointments")
 public class AppConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
     private static final String SESSION_COOKIE_NAME = "session";
 
     @Bean
+    @Order(1)
+    public FilterRegistrationBean dosFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        DoSFilter dosFilter = new DoSFilter();
+        String ipAddressOrCidrRange;
+        for (int i = 1; StringUtils.isNotBlank(ipAddressOrCidrRange = System.getenv("IP_WHITELIST_" + i)); i++) {
+            dosFilter.addWhitelistAddress(ipAddressOrCidrRange);
+            LOG.info("Adding IP address or range=[{}] to DoSFilter whitelist", ipAddressOrCidrRange);
+        }
+        registration.setFilter(dosFilter);
+        registration.addInitParameter("managedAttr", "true");  // allow filter to be managed via JMX
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+
+    @Bean
+    @Order(2)
     public FilterRegistrationBean httpsFilter() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
         registration.setFilter(new HttpsOnlyFilter());
