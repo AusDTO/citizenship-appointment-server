efb16fe11fd0d4813cf2e7def9dc622365085516
Halvard Skogsrud <halvard@skogsrud.com>
2016-02-05 14:00:41 +1100

    Basic Prometheus monitoring endpoint

---
+++ src/main/java/au/gov/dto/dibp/appointments/config/MetricsConfig.java
@@ -0,0 +1,36 @@
+package au.gov.dto.dibp.appointments.config;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import io.prometheus.client.exporter.MetricsServlet;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.actuate.endpoint.SystemPublicMetrics;
+import org.springframework.boot.actuate.metrics.Metric;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+    @Bean
+    public ServletRegistrationBean systemPublicMetricsServlet(SystemPublicMetrics publicMetrics) {
+        return new ServletRegistrationBean(createMetricsServlet(publicMetrics), "/metrics/system");
+    }
+
+    private MetricsServlet createMetricsServlet(PublicMetrics publicMetrics) {
+        CollectorRegistry collectorRegistry = CollectorRegistry.defaultRegistry;
+        MetricRegistry metricRegistry = new MetricRegistry();
+        for (Metric<?> metric : publicMetrics.metrics()) {
+            Counter counter = metricRegistry.counter(metric.getName());
+            counter.dec(counter.getCount());
+            counter.inc(Double.valueOf(metric.getValue().toString()).longValue());
+        }
+        DropwizardExports dropwizardExports = new DropwizardExports(metricRegistry);
+//        List<Collector.MetricFamilySamples> metricFamilySamples = dropwizardExports.collect();
+
+        collectorRegistry.register(dropwizardExports);
+        return new MetricsServlet(collectorRegistry);
+    }
+}
