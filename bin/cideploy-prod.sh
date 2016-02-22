2304c4fdf048028014eef9e842ba7273cf8c83a7
Emily Siow <emily.siow@digital.gov.au>
2016-02-22 15:19:07 +1100

    Use custom java-buildpack for deployment to Prod

--- bin/cideploy-prod.sh
+++ bin/cideploy-prod.sh
@@ -10,12 +10,12 @@ set -x
 # https://docs.pivotal.io/pivotalcf/devguide/deploy-apps/blue-green.html
 
 # Update the blue app
-cf unmap-route citizenship-appointment-beta-blue appointments.border.gov.au 
-cf push citizenship-appointment-beta-blue --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
-cf map-route citizenship-appointment-beta-blue appointments.border.gov.au  
+cf unmap-route citizenship-appointment-beta-blue appointments.border.gov.au
+cf push citizenship-appointment-beta-blue -b https://github.com/AusDTO/java-buildpack.git --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
+cf map-route citizenship-appointment-beta-blue appointments.border.gov.au
 
 # Update the green app
 cf unmap-route citizenship-appointment-beta-green appointments.border.gov.au
-cf push citizenship-appointment-beta-green --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
-cf map-route citizenship-appointment-beta-green appointments.border.gov.au 
+cf push citizenship-appointment-beta-green -b https://github.com/AusDTO/java-buildpack.git --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
+cf map-route citizenship-appointment-beta-green appointments.border.gov.au
 
