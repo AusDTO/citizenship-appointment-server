fd7aa298aacddc3c514b625921bb78b21d98f9a5
Michael Richardson <michael.richardson@digital.gov.au>
2016-02-24 16:27:42 +1100

    create a standard deploy script that has the URL defined as an ENV VAR

--- bin/cideploy-pws.sh
+++ bin/cideploy-pws.sh
@@ -6,17 +6,15 @@ set -e
 set -x
 
 # This is a modified version of the Cloud Foundry Blue/Green deployment guide:
+#
 # https://docs.pivotal.io/pivotalcf/devguide/deploy-apps/blue-green.html
 
-test $URL
-
 # Update the blue app
-cf unmap-route citizenship-appointment-blue $URL
+cf unmap-route citizenship-appointment-blue cfapps.io -n citizenship-appointment-beta
 cf push citizenship-appointment-blue -b https://github.com/AusDTO/java-buildpack.git --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
-cf map-route citizenship-appointment-blue $URL
+cf map-route citizenship-appointment-blue cfapps.io -n citizenship-appointment-beta
 
 # Update the green app
-cf unmap-route citizenship-appointment-green $URL
+cf unmap-route citizenship-appointment-green cfapps.io -n citizenship-appointment-beta
 cf push citizenship-appointment-green -b https://github.com/AusDTO/java-buildpack.git --no-hostname --no-manifest --no-route -p build/libs/citizenship-appointments-0.0.1.jar -i 1 -m 512M
-cf map-route citizenship-appointment-green $URL
-
+cf map-route citizenship-appointment-green cfapps.io -n citizenship-appointment-beta
