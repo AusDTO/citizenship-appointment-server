2b965577fb5b947b0d0b5fc06dbd48175584e165
Michael Richardson <michael.richardson@digital.gov.au>
2016-02-26 10:55:27 +1100

    corrected syntax of if statement

--- bin/cijavajce.sh
+++ bin/cijavajce.sh
@@ -4,7 +4,7 @@ if [ ! -d $JAVA_HOME ]; then
   cp -a $(readlink -f /opt/jdk/jdk8.latest) $JAVA_HOME
 fi
 
-if [ ! -f jce_policy-8.zip ]
+if [ ! -f jce_policy-8.zip ]; then
   curl -o jce_policy-8.zip -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
   unzip -o jce_policy-8.zip
   mv UnlimitedJCEPolicyJDK8/*.jar $JAVA_HOME/jre/lib/security
