165ca1623563b50f93e86309fbe3cf05aa2c6511
Michael Richardson <michael.richardson@digital.gov.au>
2016-02-26 10:45:30 +1100

    jdk+jce workaround for Jenkins ci builds.  JDK needs Unlimited JCE policy jar files

---
+++ bin/cijavajce.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d $JAVA_HOME ]; then
+  cp -a $(readlink -f /opt/jdk/jdk8.latest) $JAVA_HOME
+fi
+
+if [ ! -f jce_policy-8.zip ]
+  curl -o jce_policy-8.zip -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
+  unzip -o jce_policy-8.zip
+  mv UnlimitedJCEPolicyJDK8/*.jar $JAVA_HOME/jre/lib/security
+fi
+
