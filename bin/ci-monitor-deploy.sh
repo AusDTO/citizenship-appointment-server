efa0a8f11481ec4c9ba51a726024ddfa70dd7761
Michael Richardson <michael.richardson@digital.gov.au>
2016-05-10 14:02:18 +1000

    allow container service name to be specified

--- bin/ci-monitor-deploy.sh
+++ bin/ci-monitor-deploy.sh
@@ -10,9 +10,10 @@ test $AWS_ACCOUNT_ID              || failure AWS_ACCOUNT_ID
 test $AWS_REGION                  || failure AWS_REGION
 test $AWS_ACCESS_KEY_ID           || failure AWS_ACCESS_KEY_ID
 test $AWS_SECRET_ACCESS_KEY       || failure AWS_SECRET_ACCESS_KEY
-# test $DOCKERFILE_PATH             || failure DOCKERFILE_PATH
 test $DOCKER_COMPOSE_FILE_PATH    || failure DOCKER_COMPOSE_FILE_PATH
 test -f $DOCKER_COMPOSE_FILE_PATH || failure DOCKER_COMPOSE_FILE_PATH
+test $DOCKER_CONTAINER_NAME       || failure DOCKER_CONTAINER_NAME
+test $DOCKER_IMAGE_NAME           || failure DOCKER_IMAGE_NAME
 test $RABBITMQ_HOST               || failure RABBITMQ_HOST
 test $RABBITMQ_PORT               || failure RABBITMQ_PORT
 test $RABBITMQ_USER               || failure RABBITMQ_USER
@@ -29,7 +30,9 @@ sed -ie "s/IMAGE_TAG/$GIT_REF/" $DOCKER_COMPOSE_FILE_PATH
 sed -ie "s/AWS_ACCOUNT_ID/$AWS_ACCOUNT_ID/" $DOCKER_COMPOSE_FILE_PATH
 sed -ie "s/AWS_REGION/$AWS_REGION/" $DOCKER_COMPOSE_FILE_PATH
 sed -ie "s/AWS_ACCOUNT_ID/$AWS_ACCOUNT_ID/" $DOCKER_COMPOSE_FILE_PATH
+sed -ie "s/DOCKER_CONTAINER_NAME/$DOCKER_CONTAINER_NAME/" $DOCKER_COMPOSE_FILE_PATH
+sed -ie "s/DOCKER_IMAGE_NAME/$DOCKER_IMAGE_NAME/" $DOCKER_COMPOSE_FILE_PATH
 
 ecs-cli configure --cluster monitoring
-ecs-cli compose -f $DOCKER_COMPOSE_FILE_PATH service up
+ecs-cli compose --file $DOCKER_COMPOSE_FILE_PATH --project-name $DOCKER_CONTAINER_NAME service up
 
