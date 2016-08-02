a8d401dbaebdfa7c16ded442b6e93f9329be8b32
Darian Bridge <darian.border@gmail.com>
2016-08-02 12:04:58 +1000

    maintence period for friday

--- src/main/java/au/gov/dto/dibp/appointments/qflowintegration/ApiLoginService.java
+++ src/main/java/au/gov/dto/dibp/appointments/qflowintegration/ApiLoginService.java
@@ -75,9 +75,6 @@ class ApiLoginService {
         
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Australia/Sydney"));
         
-        //ZonedDateTime start = LocalDateTime.of(2016, 8, 5, 5, 00).atZone(ZoneId.of("Australia/Sydney"));
-        //ZonedDateTime end = LocalDateTime.of(2016, 8, 5, 9, 00).atZone(ZoneId.of("Australia/Sydney"));
-        
         ZonedDateTime start = LocalDateTime.of(2016, 8, 2, 12, 00).atZone(ZoneId.of("Australia/Sydney"));
         ZonedDateTime end = LocalDateTime.of(2016, 8, 2, 14, 00).atZone(ZoneId.of("Australia/Sydney"));
         
